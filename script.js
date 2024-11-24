a956992f75d4a2e6b3fda642540a33937c2876de
Dirshco <152452879+Dirshco@users.noreply.github.com>
2024-11-23 23:39:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form submission (optional validation example)
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for contacting us! We will get back to you shortly.');
+});
